16e1a8a7c5cb5cffa721ac2b34ead6e767aeb1a8
szn1995 <sujan.koju.sastra@gmail.com>
2021-12-02 08:37:33 -0600

    checkout entity stored in Db

--- src/business/Checkout.java
+++ src/business/Checkout.java
@@ -1,43 +1,65 @@
 package business;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 /**
  * @author kojusujan1111@gmail.com 01/12/21
  */
 
-public class Checkout {
+public class Checkout implements Serializable {
     private String id;
+    private String code;
     private LibraryMember member;
     private BookCopy bookCopy;
     private LocalDate checkoutDate;
     private LocalDate dueDate;
 
-    public Checkout(String id, LibraryMember member, BookCopy bookCopy, LocalDate checkoutDate, LocalDate dueDate) {
-        this.id = id;
-        this.member = member;
-        this.bookCopy = bookCopy;
-        this.checkoutDate = checkoutDate;
-        this.dueDate = dueDate;
-    }
-
     public String getId() {
         return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
     }
 
     public LibraryMember getMember() {
         return member;
     }
 
+    public void setMember(LibraryMember member) {
+        this.member = member;
+    }
+
     public BookCopy getBookCopy() {
         return bookCopy;
+    }
+
+    public void setBookCopy(BookCopy bookCopy) {
+        this.bookCopy = bookCopy;
     }
 
     public LocalDate getCheckoutDate() {
         return checkoutDate;
     }
 
+    public void setCheckoutDate(LocalDate checkoutDate) {
+        this.checkoutDate = checkoutDate;
+    }
+
     public LocalDate getDueDate() {
         return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
     }
 }
--- src/ui/CheckoutController.java
+++ src/ui/CheckoutController.java
@@ -1,9 +1,8 @@
 package ui;
 
-import business.Book;
-import business.BookCopy;
-import business.HelperUtils;
-import business.LibraryMember;
+import business.*;
+import dataaccess.DataAccess;
+import dataaccess.DataAccessFacade;
 import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -11,8 +10,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-import java.util.List;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author kojusujan1111@gmail.com 01/12/21
@@ -48,60 +48,78 @@ public class CheckoutController {
     }
 
     public void addBook() {
-        if (HelperUtils.isBlankOrNull(memberIdTextBox.getText())) {
-            showErrorAlert("Member Id empty");
-            return;
-        }
-        if (!memberIdPresent(memberIdTextBox.getText())) {
-            showErrorAlert("Member Id not found");
-            return;
-        }
-        if (HelperUtils.isBlankOrNull(isbnTextBox.getText())) {
-            showErrorAlert("ISBN empty");
-            return;
-        }
-        if (!isbnPresent(isbnTextBox.getText())) {
-            showErrorAlert("ISBN not found");
-            return;
-        }
-
-        Book book = getBookCopyWithIsbn(isbnTextBox.getText());
-        assert book != null;
-        // BookCopy bookCopy = book.getNextAvailableCopy();
+        if (validationError()) return;
+        BookCopy bookCopy = getAvailableBookCopyWithIsbn(isbnTextBox.getText());
         this.libraryMember = getMemberWithId(memberIdTextBox.getText());
-
-        //memberIdTextBox.setText("");
         isbnTextBox.setText("");
         copyNumberColumn.setCellValueFactory(new PropertyValueFactory("copyNum"));
         isbnColumn.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getBook().getIsbn()));
         titleColumn.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getBook().getTitle()));
-        // selectedBookTable.setItems(addBookToSelectedBookTable(bookCopy));
+        selectedBookTable.setItems(addBookToSelectedBookTable(bookCopy));
         showInfoAlert("Book Added Success");
         memberIdTextBox.setEditable(false);
     }
 
-    private BookCopy getAvailableCopyOfTheBook(Book book) {
-        Book b = new Book("sad", "jaava", 7, new ArrayList<>());
+    private boolean validationError() {
+        if (HelperUtils.isBlankOrNull(memberIdTextBox.getText())) {
+            showErrorAlert("Member Id empty");
+            return true;
+        }
+        if (!memberIdPresent(memberIdTextBox.getText())) {
+            showErrorAlert("Member Id not found");
+            return true;
+        }
+        if (HelperUtils.isBlankOrNull(isbnTextBox.getText())) {
+            showErrorAlert("ISBN empty");
+            return true;
+        }
+        if (!isbnPresent(isbnTextBox.getText())) {
+            showErrorAlert("ISBN not found");
+            return true;
+        }
+        return false;
+    }
 
+
+    private LibraryMember getMemberWithId(String id) {
+        //TODO IMPLEMENT
+        return new LibraryMember("suj-123", "sujan", "koju", "645241",
+                new Address("100", "fairfield", "iowa", "5227"));
+    }
+
+    private BookCopy getAvailableBookCopyWithIsbn(String isbn) {
+        //TODO IMPLEMENT
+        Book b = new Book(isbn, "java", 7, new ArrayList<>());
         return b.getCopies()[0];
     }
 
-    private LibraryMember getMemberWithId(String id) {
-        return null;
-    }
-
-    private Book getBookCopyWithIsbn(String isbn) {
-        return null;
-    }
-
     public void checkOut() {
+        DataAccess dataAccess = new DataAccessFacade();
+        LibraryMember libraryMember = this.libraryMember;
+        List<BookCopy> bookCopyList = this.bookCopyList;
+        bookCopyList.forEach(bookCopy -> {
+            String code = HelperUtils.generateUUID();
+            dataAccess.saveCheckOut(prepareCheckOutEntity(libraryMember, bookCopy, code));
+        });
         showInfoAlert("Checkout Success");
+        resetForm();
+        System.out.println("Sadsa");
+    }
+
+    private Checkout prepareCheckOutEntity(LibraryMember libraryMember, BookCopy bookCopy, String code) {
+        Checkout checkout = new Checkout();
+        checkout.setId(HelperUtils.generateUUID());
+        checkout.setCode(code);
+        checkout.setBookCopy(bookCopy);
+        checkout.setMember(libraryMember);
+        checkout.setCheckoutDate(LocalDate.now());
+        checkout.setDueDate(LocalDate.now().plusDays(bookCopy.getBook().getMaxCheckoutLength()));
+        return checkout;
     }
 
 
     private ObservableList<BookCopy> addBookToSelectedBookTable(BookCopy bookCopy) {
-        Book book = new Book("sad", "jaava", 7, new ArrayList<>());
-        this.bookCopyList.add(book.getCopies()[0]);
+        this.bookCopyList.add(bookCopy);
         return FXCollections.observableArrayList(this.bookCopyList);
     }
 
@@ -126,9 +144,15 @@ public class CheckoutController {
     }
 
     public void cancel() {
+        resetForm();
+    }
+
+    private void resetForm() {
         this.memberIdTextBox.setText("");
         this.isbnTextBox.setText("");
         this.bookCopyList = new ArrayList<>();
+        this.libraryMember = null;
         this.memberIdTextBox.setEditable(true);
+        selectedBookTable.getItems().clear();
     }
 }
